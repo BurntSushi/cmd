09e085802c5054dc1d5b75bb51663df4cdd5ee0c
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-10-31 19:26:34 -0400

    Go package docs.

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package cmd provides convenience types and functions for running shell commands
+in Go. It also includes a mechanism for running commands in parallel using
+a pool of workers.
+*/
+package cmd
